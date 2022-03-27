5b564d654e92b8872a49ba2ffc6d5ff796d7327a
Samson Kustra <66126996+RainFlyWave@users.noreply.github.com>
2022-03-27 20:21:54 +0200

    Updated creating of dirs

--- djangodockertest/asgi.py
+++ djangodockertest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for blog_base project.
+ASGI config for djangodockertest project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodockertest.settings')
 
 application = get_asgi_application()
