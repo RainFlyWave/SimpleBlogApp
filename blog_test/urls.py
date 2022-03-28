40113da3309be214eabb901cbb4b53c0b0e58cfd
Samson Kustra <s.kustra@ssm.silesia.pl>
2022-03-28 09:35:28 +0200

    Rearrange time!

--- blog_test/urls.py
+++ blog_test/urls.py
@@ -1,4 +1,4 @@
-"""djangodockertest URL Configuration
+"""blog_test URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
