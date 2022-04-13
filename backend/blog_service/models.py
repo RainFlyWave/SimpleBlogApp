817252364d890ed6fdc04bc19b3c69ebaf33a010
Samson Kustra <66126996+RainFlyWave@users.noreply.github.com>
2022-04-13 20:40:17 +0200

    Added EntryStats model

--- backend/blog_service/models.py
+++ backend/blog_service/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.timezone import now
+from django.core.validators import MaxValueValidator
 
 
 
@@ -13,4 +14,17 @@ class Entry(models.Model):
     author_name = models.ForeignKey(User, on_delete=models.CASCADE)
     blog_entry = models.TextField(max_length=512)
     date_created = models.DateTimeField(default=now)
+
+
+
+
+
+class EntryStats(models.Model):
+    """Model that stores amount of every user"""
+    
+    # entry_date_created = custom Field in progress
+    entry_author_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    entry_amount = models.IntegerField(validators=[
+        MaxValueValidator(255)
+    ])
 
