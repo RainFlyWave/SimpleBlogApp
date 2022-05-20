6fe8d63182ee504657e72eeb479f7fef176e5ef7
Samson Kustra <s.kustra@ssm.silesia.pl>
2022-05-20 09:28:32 +0200

    Created tests directory

---
+++ backend/blog_service/tests/test_urls.py
@@ -0,0 +1,14 @@
+# send requests to all urls
+# endpoints:
+    # path('', include(router.urls)),
+    # path('register/', RegisterView.as_view()),
+    # path('login/', LoginView.as_view()),
+    # path('user/', UserView.as_view()),
+    # path('logout/', LogoutView.as_view()),
+    # path('entries/',EntriesView.as_view()),
+    # path('create/',CreateEntryView.as_view()),
+    # path('delete/',DeleteEntryView.as_view()),
+    # path('edit/',EditEntryView.as_view()),
+    # path('upload/', UploadPhotoView.as_view()),
+    # path('details/', UserDescriptionView.as_view()),
+    # path('userdetails/', UserDetailsGraph.as_view()),
